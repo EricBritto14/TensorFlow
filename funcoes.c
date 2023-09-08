e1c2861c161c909c5402e8c341b3fe9b17bd428d
Eric <eric.britto22@gmail.com>
2023-09-08 20:04:59 -0300

    criaçaõ da branch

---
+++ funcoes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+}
+
+int max(int num1, int num2){
+    int resultado;
+
+    if(num1 > num2){
+        resultado = num1
+    }else{
+        resultado = num2
+    }
+
+    return resultado
+}
+
